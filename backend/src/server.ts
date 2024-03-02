8225cdda9e5caf8026f2e292035c275c8ae927bf
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-03-02 09:10:04 +0545

    delete: removed backend due to frequent crashing of the app

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { App } from '@/app';
-import { ValidateEnv } from '@utils/validateEnv';
-
-ValidateEnv();
-
-// const app = new App([new AuthRoute(), new UserRoute()]);
-const app = new App([]);
-
-app.listen();
